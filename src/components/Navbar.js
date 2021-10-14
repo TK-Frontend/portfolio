851a5650b7e7cdb48ffb06e9e94906faec4ff633
unknown <teqa@wp.pl>
2021-10-14 20:58:18 +0200

    Created file

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { FaGripLines } from "react-icons/fa";
+import tklogo from "../assets/images/tklogo.png";
+
+const Navbar = () => {
+  const toggleMobileMenu = () => {
+    const menu = document.querySelector(".mobile-menu");
+    menu.classList.toggle("hidden");
+  };
+
+  return (
+    <nav className="bg-black pt-2 font-rubik">
+      <div className="max-w-6xl mx-auto px-4 py-6 border-b border-gray-300">
+        <div className="flex justify-between items-center text-blue-50">
+          <div className="flex justify-center">
+            <img src={tklogo} alt="" className="w-16 h-16" />
+          </div>
+          <div className="flex space-x-8">
+            <div className="hidden md:flex hover:text-yellow-400 text-xl transition duration-300 ">
+              Skills
+            </div>
+            <div className="hidden md:flex hover:text-yellow-400 text-xl transition duration-300 ">
+              Portfolio
+            </div>
+            <div className="hidden md:flex hover:text-yellow-400 text-xl transition duration-300 ">
+              ContactMe
+            </div>
+          </div>
+          <div className="md:hidden flex items-center">
+            <button className="mobile-menu-button" onClick={toggleMobileMenu}>
+              <FaGripLines />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="hidden mobile-menu text-blue-50">
+        <div className=" md:hidden justify-center hover:text-yellow-400 transition duration-300 py-2">
+          Skills
+        </div>
+        <div className=" md:hidden justify-center hover:text-yellow-400 transition duration-300 py-2">
+          Portfolio
+        </div>
+        <div className=" md:hidden justify-center hover:text-yellow-400 transition duration-300 py-2">
+          ContactMe
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
