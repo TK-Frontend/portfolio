3ee7c2ee96309423164703bc5e594543f2e9c892
unknown <teqa@wp.pl>
2021-10-14 20:56:05 +0200

    Added imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ContactMe from "./components/ContactMe";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Skills from "./components/Skills";
+import ToLearn from "./components/ToLearn";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Welcome />
+      <Skills />
+      <ToLearn />
+      <Portfolio />
+      <ContactMe />
+      <Footer />
     </div>
   );
 }
