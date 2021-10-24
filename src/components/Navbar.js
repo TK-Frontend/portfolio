38a445cd5c1c19d149a77fc3fed9d1aafbb166de
unknown <teqa@wp.pl>
2021-10-24 12:51:12 +0200

    Changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,30 +10,34 @@ const Navbar = () => {
 
   return (
     <nav className="bg-black pt-2 font-rubik">
-      <div className="max-w-6xl mx-auto px-4 py-6 border-b border-gray-300">
+      <div className="max-w-6xl mx-auto px-2 sm:px-4 py-6 border-b border-gray-300">
         <div className="flex justify-between items-center text-blue-50">
           <div className="flex justify-center">
-            <img src={tklogo} alt="" className="w-16 h-16" />
+            <img
+              src={tklogo}
+              alt=""
+              className="w-14 h-auto transition-all duration-300 ease-out transform scale-100 hover:rotate-3 hover:scale-110"
+            />
           </div>
           <div className="flex space-x-8">
-            <div className="hidden md:flex hover:text-yellow-400 text-xl transition duration-300 ">
+            <div className="hidden md:flex hover:text-yellow-400 text-xl transition duration-300 cursor-pointer">
               Skills
             </div>
-            <div className="hidden md:flex hover:text-yellow-400 text-xl transition duration-300 ">
+            <div className="hidden md:flex hover:text-yellow-400 text-xl transition duration-300 cursor-pointer">
               Portfolio
             </div>
-            <div className="hidden md:flex hover:text-yellow-400 text-xl transition duration-300 ">
+            <div className="hidden md:flex hover:text-yellow-400 text-xl transition duration-300 cursor-pointer">
               ContactMe
             </div>
           </div>
           <div className="md:hidden flex items-center">
-            <button className="mobile-menu-button" onClick={toggleMobileMenu}>
+            <button className="mobile-menu-button " onClick={toggleMobileMenu}>
               <FaGripLines />
             </button>
           </div>
         </div>
       </div>
-      <div className="hidden mobile-menu text-blue-50">
+      <div className="hidden mobile-menu ease-in-out duration-700 text-blue-50">
         <div className=" md:hidden justify-center hover:text-yellow-400 transition duration-300 py-2">
           Skills
         </div>
