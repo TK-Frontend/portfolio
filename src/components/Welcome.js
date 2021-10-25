2394284f1a9cbda15b585420c8b722c0b1657223
unknown <teqa@wp.pl>
2021-10-25 10:42:33 +0200

    Changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,35 +1,54 @@
 import React from "react";
 import face from "../assets/images/imgb.png";
 import Typical from "react-typical";
+import { BsGithub } from "react-icons/bs";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const Welcome = () => {
   return (
-    <div className="bg-black p-1 md:p-8 gap-0 sm:gap-4 md:gap-6 lg:gap-8 xl:gap-10 grid grid-rows-2 sm:flex place-items-center justify-center overflow-hidden">
-      <div className="text-white font-rubik">
-        <h1 className="text-xl sm:text-2xl md:text-5xl lg:text-6xl sm:mx-20 ">
-          Hi, I&apos;m Tomasz Kozina
-        </h1>
-        <p className="text-sm sm:text-xl md:text-2xl lg:text-3xl mt-2">
-          I&apos;m
-          <Typical
-            loop={Infinity}
-            wrapper="b"
-            steps={[
-              " aspiring Frontend Developer ",
-              2000,
-              " a Father ",
-              1500,
-              " a Husband ",
-              1500,
-            ]}
-          />
-        </p>
+    <div className="bg-black text-white font-rubik">
+      <div className="max-w-6xl mx-auto gap-0 sm:gap-4 md:gap-6 lg:gap-8 xl:gap-10 grid grid-rows-3 sm:flex place-items-center justify-around overflow-hidden">
+        <div className="hidden sm:grid  grid-rows-3 place-items-center  h-96">
+          <div className="grid h-full border-r "></div>
+          <div className="">
+            <div className="grid py-2 text-2xl">
+              <BsGithub />
+            </div>
+            <div className="grid py-2 text-2xl">
+              <FaLinkedinIn />
+            </div>
+            <div className="grid py-2 text-2xl">
+              <FaFacebook />
+            </div>
+          </div>
+          <div className="grid h-full border-r "></div>
+        </div>
+        <div className="">
+          <h1 className="text-xl sm:text-2xl md:text-5xl lg:text-6xl sm:mx-20 font-bold ">
+            Hi I&apos;m Tomek
+          </h1>
+          <p className="text-sm sm:text-xl md:text-2xl lg:text-3xl mt-2 font-light">
+            <Typical
+              loop={Infinity}
+              wrapper="a"
+              steps={[
+                " I'm Frontend Developer",
+                2000,
+                "I'm a Father ",
+                1500,
+                " I'm a Husband",
+                1500,
+              ]}
+            />
+          </p>
+        </div>
+        <img
+          src={face}
+          alt="TK"
+          className="w-32 sm:w-36 md:w-48 lg:w-52 xl:w-64 2xl:w-96 transition-all duration-300 ease-out transform  hover:scale-105"
+        />
       </div>
-      <img
-        src={face}
-        alt=""
-        className="w-32 sm:w-36 md:w-48 lg:w-52 xl:w-64 2xl:w-72 transition-all duration-300 ease-out transform scale-100  hover:scale-105"
-      />
     </div>
   );
 };
