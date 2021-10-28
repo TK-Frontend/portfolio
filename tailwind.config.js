86abbac006f2f983ff106498f434f393c622ffe7
unknown <teqa@wp.pl>
2021-10-28 21:45:45 +0200

    Uninstalled plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        rubik: ['Rubik']
-      }
+        rubik: ["Rubik"],
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
