539d4d5588f249291da0ddca7790beb6d5f46f0f
unknown <teqa@wp.pl>
2021-11-02 20:10:19 +0100

    Added react-scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaGripLines } from "react-icons/fa";
 import tklogo from "../assets/images/tklogo.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const toggleMobileMenu = () => {
@@ -13,15 +14,19 @@ const Navbar = () => {
       <div className="max-w-6xl mx-auto ">
         <div className="flex justify-between items-center text-blue-50">
           <div className="flex justify-center">
-            <img
-              src={tklogo}
-              alt=""
-              className="w-16 h-auto transition-all duration-300 ease-out transform scale-100 hover:rotate-3 hover:scale-110"
-            />
+            <a rel="noreferrer" href="https://tk-frontend.github.io/portfolio/">
+              <img
+                src={tklogo}
+                alt=""
+                className="w-16 h-auto transition-all duration-300 ease-out transform scale-100 hover:rotate-3 hover:scale-110"
+              />
+            </a>
           </div>
           <div className="flex space-x-12 font-thin text-lg">
             <div className="hidden md:flex hover:text-yellow-400 text-xl transition duration-300 cursor-pointer">
-              Skills
+              <Link to="skills" smooth={true}>
+                Skills
+              </Link>
             </div>
             <div className="hidden md:flex hover:text-yellow-400 text-xl transition duration-300 cursor-pointer">
               Portfolio
@@ -39,7 +44,9 @@ const Navbar = () => {
       </div>
       <div className="hidden mobile-menu ease-in-out duration-700 text-blue-50">
         <div className=" md:hidden justify-center hover:text-yellow-400 transition duration-300 py-2">
-          Skills
+          <Link to="skills" smooth={true}>
+            Skills
+          </Link>
         </div>
         <div className=" md:hidden justify-center hover:text-yellow-400 transition duration-300 py-2">
           Portfolio
