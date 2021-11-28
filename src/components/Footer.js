cbb43a8a430f18d939854ef6496abe7c47641378
unknown <teqa@wp.pl>
2021-11-28 22:33:19 +0100

    Changed paddings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" bg-black mx-auto px-3 py-6  border-t border-gray-600 ">
-      <h1 className="text-blue-50 font-rubik sm:text-base text-xs tracking-wide">
+    <div className=" bg-black mx-auto px-3 py-6 border-t border-gray-600 ">
+      <h4 className="text-white font-rubik text-xs tracking-wide">
         Copyright © 2021 TK-Frontend. All rights reserved
-      </h1>
+      </h4>
     </div>
   );
 };
