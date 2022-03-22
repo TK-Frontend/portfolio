f8094973cbeb93cbe0a2729f73532f519710bef2
unknown <teqa@wp.pl>
2022-03-22 20:55:02 +0100

    Added two projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react/swiper-react.js";
 import { Element } from "react-scroll";
 import { BsGithub } from "react-icons/bs";
 import { FiExternalLink, FiGitlab } from "react-icons/fi";
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react.js";
+import SwiperCore, { Pagination, Navigation, Autoplay } from "swiper";
 import pizzaLogo from "../assets/images/pizzaMini.png";
 import pizzaSite from "../assets/images/pizzaSite.png";
 import starTrek from "../assets/images/starTrek.png";
 import starTrekMini from "../assets/images/starTrekMini.png";
-import SwiperCore, { Pagination, Navigation, Autoplay } from "swiper";
+import airportFinder from "../assets/images/airportFinder.png";
+import airportCircle from "../assets/images/airportCircle.png";
+import tklogo from "../assets/images/tklogo.png";
+import tkSite from "../assets/images/tkSite.png";
 import "swiper/swiper.scss";
 import "swiper/modules/navigation/navigation.scss";
 import "swiper/modules/pagination/pagination.scss";
@@ -30,12 +34,106 @@ const Projects = () => {
             slidesPerView={1}
             spaceBetween={30}
             loop={true}
-            pagination={{
-              clickable: true,
-            }}
             navigation={true}
             className="mySwiper"
           >
+            <SwiperSlide className="py-0 sm:py-10 px-8 sm:px-14 self-center">
+              <div className="flex flex-col sm:grid grid-cols-12 place-items-center h-full justify-self-start">
+                <div className="hidden sm:grid grid-rows-3 col-start-1 col-span-1 row-start-1 justify-items-center justify-self-start">
+                  <div className="grid h-full border-l"></div>
+                  <div className="py-8 ">
+                    <img src={airportCircle} alt="starTrek" className="w-10" />
+                  </div>
+                  <div className="grid h-full border-r"></div>
+                </div>
+
+                <div className="pl-0 sm:pl-2 md:pl-0 col-start-1 sm:col-start-2 col-span-7 row-start-1 z-10 text-left justify-self-start">
+                  <div className="flex flex-col justify-items-start ">
+                    <h1 className="font-semibold text-md sm:text-2xl md:text-4xl lg:text-5xl sm:py-2 text-center">
+                      Airport Finder
+                    </h1>
+                    <h2 className="text-xs sm:text-sm md:text-md lg:text-lg">
+                      Find the airport and go there!
+                    </h2>
+                    <div className="flex flex-row items-center h-10 gap-4 sm:self-start self-center">
+                      <div className="text-2xl hover:text-yellow-400 transform hover:scale-110 ">
+                        <a
+                          target="_blank"
+                          rel="noreferrer"
+                          href="https://github.com/TK-Frontend/airport-finder"
+                          className="px-2"
+                        >
+                          <BsGithub className="text-sm md:text-xl lg:text-2xl" />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-start-5 col-span-8 row-start-1 px-3 sm:pr-4">
+                  <div className="flex justify-center">
+                    <img
+                      src={airportFinder}
+                      alt=""
+                      className="w-4/5 rounded-md"
+                    />
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide className="py-0 sm:py-10 px-8 sm:px-14 self-center">
+              <div className="flex flex-col sm:grid grid-cols-12 place-items-center h-full justify-self-start">
+                <div className="hidden sm:grid grid-rows-3 col-start-1 col-span-1 row-start-1 justify-items-center justify-self-start">
+                  <div className="grid h-full border-l"></div>
+                  <div className="py-8">
+                    <img src={tklogo} alt="pizzaLogo" className="w-10" />
+                  </div>
+                  <div className="grid h-full border-r"></div>
+                </div>
+
+                <div className="pl-0 sm:pl-2 md:pl-0 col-start-1 sm:col-start-2 col-span-7 row-start-1 z-10 text-left justify-self-start">
+                  <div className="flex flex-col justify-items-start">
+                    <h1 className="font-semibold text-md sm:text-2xl md:text-4xl lg:text-5xl sm:py-2 text-center sm:text-left">
+                      Portfolio Site
+                    </h1>
+                    <h2 className="text-xs sm:text-sm md:text-md lg:text-lg">
+                      Some information about me
+                    </h2>
+                    <div className="flex flex-row items-center h-10 gap-4 sm:self-start self-center">
+                      <div className="text-2xl hover:text-yellow-400 transform hover:scale-110">
+                        <a
+                          target="_blank"
+                          rel="noreferrer"
+                          href="https://github.com/TK-Frontend/findyourpizza"
+                          className="px-2"
+                        >
+                          <BsGithub className="text-sm md:text-xl lg:text-2xl" />
+                        </a>
+                      </div>
+                      <div className="text-2xl hover:text-yellow-400 transform hover:scale-110">
+                        <a
+                          target="_blank"
+                          rel="noreferrer"
+                          href="https://tk-frontend.github.io/findyourpizza"
+                        >
+                          <FiExternalLink className="text-sm md:text-xl lg:text-2xl" />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-start-5 col-span-8 row-start-1 px-3 sm:pr-4">
+                  <img
+                    src={tkSite}
+                    alt=""
+                    className="w-full rounded-md opacity-90"
+                  />
+                </div>
+              </div>
+            </SwiperSlide>
+
             <SwiperSlide className="py-0 sm:py-10 px-8 sm:px-14 self-center">
               <div className="flex flex-col sm:grid grid-cols-12 place-items-center h-full justify-self-start">
                 <div className="hidden sm:grid grid-rows-3 col-start-1 col-span-1 row-start-1 justify-items-center justify-self-start">
@@ -89,7 +187,7 @@ const Projects = () => {
             </SwiperSlide>
 
             <SwiperSlide className="py-0 sm:py-10 px-8 sm:px-14 self-center">
-              <div className="flex flex-col sm:grid grid-cols-12 place-items-center h-full justify-self-start">
+              <div className="flex flex-col sm:grid grid-cols-12 place-items-center h-full justify-self-start ">
                 <div className="hidden sm:grid grid-rows-3 col-start-1 col-span-1 row-start-1 justify-items-center justify-self-start">
                   <div className="grid h-full border-l"></div>
                   <div className="py-8 ">
