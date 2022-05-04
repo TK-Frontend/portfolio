83c3f5b92537972ff30b433073a3b3f98979c03d
unknown <teqa@wp.pl>
2022-05-04 20:41:25 +0200

    Added HTML5 tags

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -2,11 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" bg-black mx-auto px-3 py-6 border-t border-gray-600 ">
-      <h4 className="text-white font-rubik text-xs tracking-wide">
-        Copyright © 2021 TK-Frontend. All rights reserved
-      </h4>
-    </div>
+    <section>
+      <footer className=" bg-black mx-auto px-3 py-6 border-t border-gray-600 ">
+        <h4 className="text-white font-rubik text-xs tracking-wide">
+          Copyright © 2021 TK-Frontend. All rights reserved
+        </h4>
+      </footer>
+    </section>
   );
 };
 
